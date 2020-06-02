f216586a46dee0c844b1eaaa58052224619ed653
DEVRAJ <devraj@Deeps-MacBook-Air.local>
2020-06-02 14:00:05 +0545

    inistial

---
+++ script.js
@@ -0,0 +1,8 @@
+const work = document.querySelectorAll(".Work")
+// console.log(work)
+work[0].addEventListener("click",	()=>{
+	alert("working on it")
+})
+work[1].addEventListener("click",	()=>{
+	alert("working on it")
+})
